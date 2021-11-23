183f859e23abd5dddb9c21f8b2d50e3bf99e297d
Martin PCescritorio <bergesio90@gmail.com>
2021-11-23 21:55:56 +0100

    Empezando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div>
+      <h1>hola</h1>
+    </div>
+  );
+};
 
 export default App;
