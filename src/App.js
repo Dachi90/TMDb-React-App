e85a07aa1b491ff28c38e6d9ee569fad76438488
Martin Bergesio Trabajo <bergesio90@gmai.com>
2021-11-25 11:16:29 +0100

    Busqueda

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   const TRENDING_URL = "https://api.themoviedb.org/3/trending/all/week?api_key=73b7f93676cf548e83b720fd7de7b73b&language=es-ES";
+  const SEARCH_URL = "https://api.themoviedb.org/3/search/multi?api_key=73b7f93676cf548e83b720fd7de7b73b&language=es-ES&query=";
 
   const apiFetch = (url) => {
     fetch(url)
@@ -20,14 +21,27 @@ const App = () => {
     apiFetch(TRENDING_URL);
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userSearchInput = document.getElementById("search").value.trim().toLowerCase();
+    console.log(userSearchInput);
+
+    console.log(SEARCH_URL + userSearchInput);
+    if (!userSearchInput) {
+      return;
+    }
+    apiFetch(SEARCH_URL + userSearchInput);
+  };
+
   return (
     <div className="appDiv">
       <header className="header">
         <div className="logoContainer">
           <img src={logo} alt="logo" className="logo" />
         </div>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input type="search" name="search" id="search" className="inputForm" />
+          <button type="submit">Buscar</button>
         </form>
       </header>
       <main className="mainContent">
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
+import image from "../assets/image-not-found.png";
+
 const Card = ({ data }) => {
   //console.log(data);
   const POSTER_PATH = "https://image.tmdb.org/t/p/w500";
   return (
     <div className="card">
-      <img src={POSTER_PATH + data.poster_path} alt="portada" />
+      <img src={data.poster_path ? POSTER_PATH + data.poster_path : image} alt="portada" />
       <div className="movieDetails">
         <p>{data.title ? data.title : data.name}</p>
         <p>{data.vote_average}</p>
